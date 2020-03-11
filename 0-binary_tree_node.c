7b0d70ad0ba5e180732fdba85a8c20dbb7451e05
towasme <towasm@hmail.com>
2020-03-10 23:58:48 -0500

    starting with task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ *listint_len - prints a list
+ *@h: The character to print
+ *Return: On success 1.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    
+}
