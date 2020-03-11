f687f081dcff8a7c2f31c885b6c81045e9fca0e9
towasme <towasm@gmail.com>
2020-03-11 17:14:15 -0500

    func that checks for leafs

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - creates a bianry tree
+ *@node: checks this node if its a leaf
+ *Return: On success 1.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node->left || node->right)
+    {
+        return (0);
+    }
+    return (1);
+    
+}
