00d5f47c4f3532fb7a159c37471b2fa821bdcd0d
fruizga <ruizgallego@gmail.com>
2020-03-11 17:25:53 -0500

    if node is NULL

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,6 +6,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+    if (node == NULL)
+    {
+            return (0);
+    }
+    
     if (node->left || node->right)
     {
         return (0);
