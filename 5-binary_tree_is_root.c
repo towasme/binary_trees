ecca7f9191378cc1da63be8b28b1be5f8420459d
fruizga <ruizgallego@gmail.com>
2020-03-11 17:23:15 -0500

    checks if node is a root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - creates a bianry tree
+ *@node: checks this node if its a leaf
+ *Return: On success 1.
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node->parent == NULL)
+    {
+        return (1);
+    }
+    return (0);
+}
