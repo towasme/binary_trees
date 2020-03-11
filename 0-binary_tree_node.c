50309ddf2f2f7818cbc7a1843123834e98096ebe
towasme <towasm@gmail.com>
2020-03-11 12:11:14 -0500

    function that creates a node, and constructing funct that adds a node to the left

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- *listint_len - prints a list
- *@h: The character to print
+ *binary_tree_node - creates a bianry tree
+ *@parent: The node to add to parent tree
+ *@value: value the node will have
  *Return: On success 1.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    
+    binary_tree_t *new;
+
+    new = malloc(sizeof(binary_tree_t));
+    if (new == NULL)
+        return NULL;
+    new->n = value;
+    new->left = NULL;
+    new->right = NULL;
+    new->parent = parent;
+    if (parent == NULL)
+        {
+            parent = new;
+        }
+    return (new);
 }
---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert - creates a bianry tree
+ *@parent: The node to add to parent tree
+ *@value: value the node will have
+ *Return: On success 1.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_left;
+
+    if (parent == NULL)
+        return (NULL);
+    new_left = malloc(sizeof(binary_tree_t));
+    if (new_left == NULL)
+        return (NULL);
+    
+    new_left->n = value;
+    new_left->left = NULL;
+    new_left->right = NULL;
+    new_left->parent = parent;
+    if (parent->left != NULL)
+    {
+        new_left->left = parent->left;
+        parent->left = new_left;
+    }
+    else
+    {
+        parent->left = new_left;
+    }
+    return (new_left);
+}
