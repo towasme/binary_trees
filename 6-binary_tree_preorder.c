736ead35d1dd7584e02c80c0dd01cbd872ff9521
fruizga <ruizgallego@gmail.com>
2020-03-11 18:36:52 -0500

    -m

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,10 +6,10 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-	{
+if (tree == NULL || func == NULL)
+{
  return;
-	}
+}
 func(tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
