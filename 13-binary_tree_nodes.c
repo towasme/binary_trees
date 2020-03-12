6520690795250c7c3ac62f0338bb88be2c77288a
towasme <towasm@gmail.com>
2020-03-12 18:05:15 -0500

    tasks from 13 to 15

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,4 +8,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	    return (0);
-
+	if (tree->left || tree->right)
+	{
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	}
+	return (0);
+}
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+* * binary_tree_balance - looks for the depth of the tree
+* @tree: pointer to the node
+* Return: On success 1
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+}
