083f5844be09799562efa658bcf2535ea67ed39e
fruizga <ruizgallego@gmail.com>
2020-03-13 00:34:34 -0500

    applying betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,14 +6,14 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    
-    if (!tree->left && !tree->right)
-        return (1);
+if (tree == NULL)
+return (0);
 
-    if ((tree->right) && (tree->left))
-        return (binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)));
-    
-    return (0);
+if (!tree->left && !tree->right)
+return (1);
+
+if ((tree->right) && (tree->left))
+return (binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)));
+
+return (0);
 }
