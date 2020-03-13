071975d2e5a047d16bb7f5890158b5c079bad014
fruizga <ruizgallego@gmail.com>
2020-03-13 14:51:36 -0500

    18 binary

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,11 +6,11 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL
-		|| node->parent->parent == NULL)
+	if (node == NULL
+		||  node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->right)
+	if (node->parent->parent->right == node->parent)
 	{
 		return (node->parent->parent->left);
 	}
