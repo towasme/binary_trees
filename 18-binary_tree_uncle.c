c7136cfa20c093a97cc11fa11069ef4a60ff4465
fruizga <ruizgallego@gmail.com>
2020-03-13 14:47:14 -0500

    cheking subtree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,10 +10,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		|| node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent == node->parent->parent->left)
+	if (node->parent == node->parent->parent->right)
 	{
-		return (node->parent->parent->right);
+		return (node->parent->parent->left);
 	}
-	return (node->parent->parent->left);
+	return (node->parent->parent->right);
 }
-
