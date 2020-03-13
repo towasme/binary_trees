fd3b67a427313ab5c8be434434a91dcd69f06713
towasme <towasm@gmail.com>
2020-03-13 00:00:46 -0500

    func that looks por full binary trees

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,38 +1,19 @@
 #include "binary_trees.h"
 /**
-* * binary_tree_leaves - looks for the depth of the tree
-* @tree: pointer to the node
-* Return: On success 1
-*/
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-    if (tree == NULL)
-        return (0);
-
-    if (!tree->left && !tree->right)
-    {
-        return (1);
-    }
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
-
-/**
 * * binary_tree_is_full - looks for the depth of the tree
 * @tree: pointer to the node
 * Return: On success 1
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    size_t par = 0;
-
     if (tree == NULL)
         return (0);
     
-    par = binary_tree_leaves(tree);
-    if (par % 2)
-        return (0);
-    else
-    {
+    if (!tree->left && !tree->right)
         return (1);
-    }   
+
+    if ((tree->right) && (tree->left))
+        return (binary_tree_is_full(tree->left) && (binary_tree_is_full(tree->right)));
+    
+    return (0);
 }
