b47620bbb5742ebe15c2d91aca00f65eb83e680b
towasme <towasm@gmail.com>
2020-03-13 14:00:40 -0500

    betty correction 13 and 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,18 +6,13 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node->parent || !node || !node->parent->parent
-		       || !node->parent->parent->left
-		       || !node->parent->parent->right)
+	if (!node->parent || !node || !node->parent->parent)
 		return (NULL);
 
 	if (node->parent == node->parent->parent->left)
 	{
 		return (node->parent->parent->right);
 	}
-	else
-	{
-		return (node->parent->parent->left);
-	}
+	return (node->parent->parent->left);
 }
 
